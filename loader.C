13da88dc8da2e57b1747f1369e4eade63940ccfe
giopolykra <giopolykra@gmail.com>
2019-06-15 23:38:17 +0200

    first commit

---
+++ loader.C
@@ -0,0 +1,25 @@
+{
+ gROOT->ProcessLine(".x selection_0.C");
+ gROOT->ProcessLine(".x selection_1.C");
+ gROOT->ProcessLine(".x selection_2.C");
+ gROOT->ProcessLine(".x selection_3.C");
+ gROOT->ProcessLine(".x selection_4.C");
+ gROOT->ProcessLine(".x selection_5.C");
+ gROOT->ProcessLine(".x selection_6.C");
+ gROOT->ProcessLine(".x selection_7.C");
+ gROOT->ProcessLine(".x selection_8.C");
+ gROOT->ProcessLine(".x selection_9.C");
+ gROOT->ProcessLine(".x selection_10.C");
+ gROOT->ProcessLine(".x selection_11.C");
+ gROOT->ProcessLine(".x selection_12.C");
+ gROOT->ProcessLine(".x selection_13.C");
+ gROOT->ProcessLine(".x selection_14.C");
+ gROOT->ProcessLine(".x selection_15.C");
+ gROOT->ProcessLine(".x selection_16.C");
+ gROOT->ProcessLine(".x selection_17.C");
+ gROOT->ProcessLine(".x selection_18.C");
+ gROOT->ProcessLine(".x selection_19.C");
+ gROOT->ProcessLine(".x selection_20.C");
+ gROOT->ProcessLine(".x selection_21.C");
+}
+# Ma5-FastJet
